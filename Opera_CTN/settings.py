64f8768801cb094672a57a28e33afb8b9c0f5c34
dairoun njankouo <dairounjankouo2019@gmail.com>
2023-08-21 11:05:30 +0200

    desuxieme commit

--- Opera_CTN/settings.py
+++ Opera_CTN/settings.py
@@ -104,7 +104,7 @@ WSGI_APPLICATION = 'Opera_CTN.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -123,7 +123,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
