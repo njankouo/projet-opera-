91f543ce7dab1ccff3cd9ab30336d271b3289170
dairoun njankouo <dairounjankouo2019@gmail.com>
2023-08-22 02:07:21 +0200

    nouveau commit de opera+

--- Opera_CTN/settings.py
+++ Opera_CTN/settings.py
@@ -104,7 +104,7 @@ WSGI_APPLICATION = 'Opera_CTN.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -116,14 +116,14 @@ DATABASES = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'nkoldongo_opera',
+        'NAME': 'opera_sql',
         'USER': 'postgres',
-        'PASSWORD': 'admin',
+        'PASSWORD': 'corporate2019',
         'HOST': 'localhost',
         'PORT': '5432',
     }
 }
-"""
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
